e9085c950641efb264462a14fe1d1be2448f2018
AdilAggne <129189944+AdilAggne@users.noreply.github.com>
2023-08-10 09:49:57 +0530

    Initial commit

---
+++ projects/host-app/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { TodoListModule } from 'projects/mfe-app/src/app/todo-list/todo-list.module';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    TodoListModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
